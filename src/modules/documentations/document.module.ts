39ad031e1268d9b79cab8b665ee0803a2dd6fb0a
ACER <ahmadillohasanov099@gmail.com>
2025-06-05 17:53:47 +0500

    sms nodemailer completed

--- src/modules/documentations/document.module.ts
+++ src/modules/documentations/document.module.ts
@@ -3,10 +3,11 @@ import { DocumentationController } from './document.controlller';
 import { DocumentationService } from './document.service';
 import { PrismaService } from 'src/prisma';
 import { UserModule } from '../user-auth';
+import { MailService } from 'src/common';
 
 @Module({
   imports: [UserModule],
   controllers: [DocumentationController],
-  providers: [DocumentationService, PrismaService],
+  providers: [DocumentationService, PrismaService,MailService],
 })
 export class DocumentationModule {}
